da775c49ca27df232abc2d77f444ad3471b679c5
Steven <stevenulbrich@me.com>
2020-02-29 07:19:16 -0600

    Update Index Motor Settings

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,20 +7,21 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.shuffleboard.*;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.*;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
@@ -30,13 +31,22 @@ public class Robot extends TimedRobot {
   private Joystick m_stick;
   private static final int TopMotorId = 5;
   private static final int BottomMotorID = 6;
+  private static final int IndexMotorID = 7;
   private CANSparkMax m_motorTop;
   private CANSparkMax m_motorBottom;
+  private WPI_TalonFX m_indexMotor;
   private CANPIDController m_pidController;
   private CANPIDController m_pidControllerBottom;
   private CANEncoder m_encoder;
   private CANEncoder m_encoderBottom;
   public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+
+  public double maxRPM_Top, maxRPM_Bottom, kMaxRPM_Top, kMaxRPM_Bottom;
+
+  public static String cnameMaxRpmTop = "Top Motor Max RPM";
+  public static String cnameMaxRpmBottom = "Bottom Motor Max RPM";
+
+
 
   @Override
   public void robotInit() {
@@ -45,6 +55,10 @@ public class Robot extends TimedRobot {
     // initialize motor
     m_motorTop = new CANSparkMax(TopMotorId, MotorType.kBrushless);
     m_motorBottom = new CANSparkMax(BottomMotorID, MotorType.kBrushless);
+    m_indexMotor = new WPI_TalonFX(IndexMotorID);
+
+
+
     /**
      * The RestoreFactoryDefaults method can be used to reset the configuration
      * parameters in the SPARK MAX to their factory default state. If no argument is
@@ -56,7 +70,11 @@ public class Robot extends TimedRobot {
     m_motorTop.setIdleMode(IdleMode.kCoast);
     m_motorBottom.setIdleMode(IdleMode.kCoast);
 
-    m_motorBottom.setInverted(true);
+    m_motorTop.setInverted(true);
+
+    m_indexMotor.set(ControlMode.PercentOutput, 0);
+    m_indexMotor.configFactoryDefault();
+    m_indexMotor.setNeutralMode(NeutralMode.Brake);
 
     /**
      * In order to use PID functionality for a controller, a CANPIDController object
@@ -78,7 +96,15 @@ public class Robot extends TimedRobot {
     kFF = 0.000015;
     kMaxOutput = 1;
     kMinOutput = -1;
+    kMaxRPM_Top = 15000;
+    kMaxRPM_Bottom = 15000;
     maxRPM = 15000;
+    maxRPM_Bottom = 15000;
+    maxRPM_Top = 15000;
+
+
+    boolean[] _previousBtns = new boolean[10];
+    boolean[] _currentBtns = new boolean[10];
 
     // set PID coefficients
     m_pidController.setP(kP);
@@ -104,60 +130,44 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Feed Forward", kFF);
     SmartDashboard.putNumber("Max Output", kMaxOutput);
     SmartDashboard.putNumber("Min Output", kMinOutput);
-    SmartDashboard.putNumber("SetPoint", 0);
-    SmartDashboard.putNumber("Velocity", 0);
 
-   
+    SmartDashboard.putNumber("SetPoint-TopMotor", 0);
+    SmartDashboard.putNumber("RPM-TopMotor", 0);
+    SmartDashboard.putNumber(cnameMaxRpmTop, kMaxRPM_Top);
 
-
+    SmartDashboard.putNumber("SetPoint-BottomMotor", 0);
+    SmartDashboard.putNumber("RPM-BottomMotor", 0);
+    SmartDashboard.putNumber(cnameMaxRpmBottom, kMaxRPM_Bottom);
 
   }
 
   @Override
   public void teleopPeriodic() {
+
     // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-    double i = SmartDashboard.getNumber("I Gain", 0);
-    double d = SmartDashboard.getNumber("D Gain", 0);
-    double iz = SmartDashboard.getNumber("I Zone", 0);
-    double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    double max = SmartDashboard.getNumber("Max Output", 0);
-    double min = SmartDashboard.getNumber("Min Output", 0);
+    /**
+     * final double p = SmartDashboard.getNumber("P Gain", 0); final double i =
+     * SmartDashboard.getNumber("I Gain", 0); final double d =
+     * SmartDashboard.getNumber("D Gain", 0); final double iz =
+     * SmartDashboard.getNumber("I Zone", 0); final double ff =
+     * SmartDashboard.getNumber("Feed Forward", 0);
+     */
 
-    // if PID coefficients on SmartDashboard have changed, write new values to
-    // controller
-    if ((p != kP)) {
-      m_pidController.setP(p);
-      m_pidControllerBottom.setP(p);
-      kP = p;
-    }
-    if ((i != kI)) {
-      m_pidController.setI(i);
-      m_pidControllerBottom.setI(i);
-      kI = i;
-    }
-    if ((d != kD)) {
-      m_pidController.setD(d);
-      m_pidControllerBottom.setD(d);
-      kD = d;
-    }
-    if ((iz != kIz)) {
-      m_pidController.setIZone(iz);
-      m_pidControllerBottom.setIZone(iz);
-      kIz = iz;
-    }
-    if ((ff != kFF)) {
-      m_pidController.setFF(ff);
-      m_pidControllerBottom.setFF(ff);
-      kFF = ff;
-    }
-    if ((max != kMaxOutput) || (min != kMinOutput)) {
-      m_pidController.setOutputRange(min, max);
-      m_pidControllerBottom.setOutputRange(min, max);
-      kMinOutput = min;
-      kMaxOutput = max;
+
+
+
+    if (maxRPM_Top != kMaxRPM_Top){
+        kMaxRPM_Top= maxRPM_Top;
     }
 
+    if (maxRPM_Bottom != kMaxRPM_Bottom){
+        kMaxRPM_Bottom = maxRPM_Bottom;
+    }
+
+    final double maxRPM_Top = SmartDashboard.getNumber(cnameMaxRpmTop, kMaxRPM_Bottom);
+    final double maxRPM_Bottom = SmartDashboard.getNumber(cnameMaxRpmBottom, kMaxRPM_Bottom);
+    
+ 
     /**
      * PIDController objects are commanded to a set point using the SetReference()
      * method.
@@ -170,11 +180,24 @@ public class Robot extends TimedRobot {
      * com.revrobotics.ControlType.kPosition com.revrobotics.ControlType.kVelocity
      * com.revrobotics.ControlType.kVoltage
      */
-    double setPoint = m_stick.getY() * maxRPM;
-    m_pidController.setReference((setPoint * .8), ControlType.kVelocity);
-    m_pidControllerBottom.setReference(setPoint , ControlType.kVelocity);
+    final double m_stickY = Math.abs(m_stick.getY());
 
-    SmartDashboard.putNumber("SetPoint", setPoint);
-    SmartDashboard.putNumber("Velocity", m_encoder.getVelocity());
+    final double setPointTop = m_stickY * maxRPM_Top;
+    final double setPointBottom = m_stickY * maxRPM_Bottom;
+
+    m_pidController.setReference((setPointTop), ControlType.kVelocity);
+    m_pidControllerBottom.setReference(setPointBottom, ControlType.kVelocity);
+
+    SmartDashboard.putNumber("SetPoint Top",(setPointTop));
+    SmartDashboard.putNumber("RPM Raw Motor Top", Math.abs(m_encoder.getVelocity()));
+
+    SmartDashboard.putNumber("SetPoint Bottom", (setPointBottom));
+    SmartDashboard.putNumber("RPM Raw Motor Bottom", Math.abs(m_encoderBottom.getVelocity()));
+
+    //m_stick.getRawButtonPressed(button)
+    m_indexMotor.set(ControlMode.PercentOutput, 0.20);
+    
+    
   }
+
 }
